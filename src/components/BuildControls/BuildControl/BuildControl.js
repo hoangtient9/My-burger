e32b00475226114bd7139e7193904f22819303b2
hoangtjent9 <hoangtjent9@gmail.com>
2020-06-03 00:04:04 +0700

    Fixed BuildControls

--- src/components/BuildControls/BuildControl/BuildControl.js
+++ src/components/BuildControls/BuildControl/BuildControl.js
@@ -2,10 +2,10 @@ import React from 'react';
 import classes from './BuildControl.module.scss';
 
 const BuildControl = props => (
-  <div className={classes.Control}>
-    <div className={classes.Label}>{'Cheese'}</div>
-    <button className={classes.Less}>{'Less'}</button>
-    <button className={classes.More}>{'More'}</button>
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>{props.label}</div>
+    <button className={classes.Less}>Less</button>
+    <button className={classes.More}>More</button>
   </div>
 )
 
--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -2,9 +2,15 @@ import React from 'react';
 import classes from './BuildControls.module.scss';
 import BuildControl from './BuildControl/BuildControl';
 
+const controls = [
+  {label: 'Bacon', type: 'bacon'},
+  {label: 'Cheese', type: 'cheese'},
+  {label: 'Meat', type: 'meat'},
+  {label: 'Salad', type: 'salad'},
+]
 const BuildControls = props => (
-  <div className={classes.Controls}>
-    <BuildControl />
+  <div className={classes.BuildControls}>
+    {controls.map(ctr => <BuildControl key={ctr.label} label={ctr.label} />)}
   </div>
 )
 
