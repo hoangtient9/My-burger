5d665852a49d6d92b76976da4e52aa8b2063528c
hoangtjent9 <hoangtjent9@gmail.com>
2020-06-05 00:19:17 +0700

    Add Sidedrawer component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.scss';
 import Tollbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const Layout = props => (
   <Aux>
+    <SideDrawer />
     <Tollbar />
     <main className={classes.Content}>
         {props.children}
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import classes from './Toolbar.module.scss';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import NavigationItem from '../NavigationItems/NavigationItem/NavigationItem';
 
 const tollbar = (props) => (
-    <header className={classes.Toolbar}>
-        <div>Menu</div>
-        <Logo />
-        <nav>Order</nav>
-    </header>
+  <header className={classes.Toolbar}>
+    <div>Menu</div>
+    <Logo />
+    <nav>
+      <NavigationItems />
+    </nav>
+  </header>
 );
 
 export default tollbar;
