7f3bf614b375dcbb3c9f93e7ba09d9d702993ad9
hoangtjent9 <hoangtjent9@gmail.com>
2020-07-06 23:54:27 +0700

    Updates

---
+++ precache-manifest.459b5a376d10548a00c601df8643d07f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9300343de9ec73f04443bb3a35a1fe60",
+    "url": "/My-burger/index.html"
+  },
+  {
+    "revision": "f3e33bf474847fa3547f",
+    "url": "/My-burger/static/css/3.2390ad0c.chunk.css"
+  },
+  {
+    "revision": "4e3fe942fe8800f88d9f",
+    "url": "/My-burger/static/css/4.08b8c1a6.chunk.css"
+  },
+  {
+    "revision": "cba8d87c5cabaa6b6d99",
+    "url": "/My-burger/static/css/5.6f314fb0.chunk.css"
+  },
+  {
+    "revision": "419bd63a9517586b5ea4",
+    "url": "/My-burger/static/css/main.ce84a265.chunk.css"
+  },
+  {
+    "revision": "02cc68a5a11db9693955",
+    "url": "/My-burger/static/js/2.71a953e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-burger/static/js/2.71a953e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3e33bf474847fa3547f",
+    "url": "/My-burger/static/js/3.41af39f9.chunk.js"
+  },
+  {
+    "revision": "4e3fe942fe8800f88d9f",
+    "url": "/My-burger/static/js/4.41a29f40.chunk.js"
+  },
+  {
+    "revision": "cba8d87c5cabaa6b6d99",
+    "url": "/My-burger/static/js/5.0bba409c.chunk.js"
+  },
+  {
+    "revision": "419bd63a9517586b5ea4",
+    "url": "/My-burger/static/js/main.e47d1569.chunk.js"
+  },
+  {
+    "revision": "0c7d8f3cf61ca6e718a2",
+    "url": "/My-burger/static/js/runtime-main.d7a18f71.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/My-burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
